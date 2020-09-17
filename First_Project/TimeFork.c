5bfad8bacfae852ff442ce2af73d1ae4206a3aad
saavidhingra <saavidhingra@gmail.com>
2020-09-17 00:44:09 -0400

    Added Code to TimeFork and SysCall

--- First_Project/TimeFork.c
+++ First_Project/TimeFork.c
@@ -1,15 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-//Place any necessary global variables here
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]){
-    /*
-     *
-     * Implement Here
-     * 
-     * 
-     * 
-     */
-    return 0;    
+
+	int numForks = 5000;
+	double avgTime = 0;
+	pid_t cpid;
+	struct timeval start, end, avgStart, avgEnd;
+
+	gettimeofday(&start, NULL);
+	for (int i = 0; i < 5000; i++) {
+		gettimeofday(&avgStart, NULL);
+		if (fork() == 0) {
+			exit(0);
+		} else {
+			wait(NULL);
+		}
+		gettimeofday(&avgEnd, NULL);
+
+		double avgSeconds = (avgEnd.tv_sec - avgStart.tv_sec);
+		double avgMicroseconds = ((avgSeconds * 1000000) + avgEnd.tv_usec) - (avgStart.tv_usec);
+		avgTime += avgMicroseconds;
+	}
+	gettimeofday(&end, NULL);
+
+	avgTime = avgTime/numForks;
+
+	double seconds = (end.tv_sec - start.tv_sec);
+	double microseconds = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+
+	printf("Forks Performed: %d\n", numForks);
+	printf("Total Elapsed Time: %f microseconds\n", microseconds);
+	printf("Average Time Per Fork: %f microseconds\n", avgTime);
+
+	return 0;
 }
--- First_Project/TimeSysCall.c
+++ First_Project/TimeSysCall.c
@@ -1,15 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-//Place any necessary global variables here
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]){
-    /*
-     *
-     * Implement Here
-     * 
-     * 
-     * 
-     */
-    return 0;    
+
+	int numSysCall = 100000;
+	double avgTime = 0;
+	pid_t process_id;
+	struct timeval start, end, avgStart, avgEnd;
+
+	gettimeofday(&start, NULL);
+	for (int i = 0; i < 100000; i++) {
+		gettimeofday(&avgStart, NULL);
+		process_id = getpid();
+		gettimeofday(&avgEnd, NULL);
+		double avgSeconds = (avgEnd.tv_sec - avgStart.tv_sec);
+		double avgMicroseconds = ((avgSeconds * 1000000) + avgEnd.tv_usec) - (avgStart.tv_usec);
+		avgTime += avgMicroseconds;
+	}
+	gettimeofday(&end, NULL);
+
+	double seconds = (end.tv_sec - start.tv_sec);
+	double microseconds = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+	avgTime = avgTime/numSysCall;
+
+	printf("Syscalls Performed: %d\n", numSysCall);
+	printf("Total Elapsed Time: %f microseconds\n", microseconds);
+	printf("Average Time Per Syscall: %f microseconds\n", avgTime);
+
+	return 0;
 }
